e7e1c09cc035e98e5b702bb78a5001bb556e532c
PriscilaCamisao <priscila.camisao@gmail.com>
2022-04-02 00:18:32 -0300

    Layout entrar e cadastrar

--- BLOCO III/Blog Pessoal/BlogPessoal/src/app/app.component.ts
+++ BLOCO III/Blog Pessoal/BlogPessoal/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 @Component({
 
@@ -6,6 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'BlogPessoal';
+  
+  constructor(
+    public auth: AuthService
+  ){}
+
 }
